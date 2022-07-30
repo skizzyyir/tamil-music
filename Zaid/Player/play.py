24f98f925e67ba1485818583bb1d9bee0c42bc30
skizzyyir <109980951+skizzyyir@users.noreply.github.com>
2022-07-30 15:58:38 +0530

    Update play.py

--- Zaid/Player/play.py
+++ Zaid/Player/play.py
@@ -141,7 +141,7 @@ async def play(c: Client, m: Message):
                       InlineKeyboardButton("▶️", callback_data="cbresume"),
                   ],[
                       InlineKeyboardButton(text="✨ ɢʀᴏᴜᴘ", url=f"https://t.me/{GROUP_SUPPORT}"),
-                      InlineKeyboardButton(text="📣 ᴄʜᴀɴɴᴇʟ", url=f"https://t.me/{UPDATES_CHANNEL}"),
+                      InlineKeyboardButton(text="🥀 ᴏᴡɴᴇʀ", url=f"https://t.me/Suryaakumar"),
                   ],[
                       InlineKeyboardButton("🗑", callback_data="cls")],
                   ]
