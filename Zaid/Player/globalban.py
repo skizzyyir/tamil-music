e19568daf73eed46e0e0a2f3c7a3943b5f63a948
skizzyyir <109980951+skizzyyir@users.noreply.github.com>
2022-08-01 06:06:27 +0530

    Update globalban.py

--- Zaid/Player/globalban.py
+++ Zaid/Player/globalban.py
@@ -15,16 +15,16 @@ from pyrogram.types import Message
 
 from config import BANNED_USERS
 from strings import get_command
-from YukkiMusic import app
-from YukkiMusic.misc import SUDOERS
-from YukkiMusic.utils import get_readable_time
-from YukkiMusic.utils.database import (add_banned_user,
+from Zaid import app
+from Zaid.misc import SUDOERS
+from Zaid.utils import get_readable_time
+from Zaid.utils.database import (add_banned_user,
                                        get_banned_count,
                                        get_banned_users,
                                        get_served_chats,
                                        is_banned_user,
                                        remove_banned_user)
-from YukkiMusic.utils.decorators.language import language
+from Zaid.utils.decorators.language import language
 
 # Command
 GBAN_COMMAND = get_command("GBAN_COMMAND")
